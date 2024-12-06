414f4ba72abb251d36b1aaeb691e0a00d81660fb
Rosalie Nicaud <rosalie@MacBook-Pro-de-Rosalie.local>
2024-12-06 12:01:34 +0100

    Réorganisation fichiers

---
+++ front/EtapesPersonnalisation/etape4.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../../styles/processus.css">
+  
+  <title>Étape 1 - Choisi ta structure</title>
+</head>
+<body>
+
+<header>
+  <?php require '../../squelette/header.php'; ?>
+</header>
+
+<main>
+  <div class="container">
+    <!-- Colonne de gauche -->
+    <div class="left-column">
+      <h2>Étape 1 - Choisi ta structure</h2>
+      
+      <section class="color-options">
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Armoire">
+          <p>Armoire</p>
+          <span>20 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Tissu">
+          <p>Tissu</p>
+          <span>30 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Torsade">
+          <p>Torsade</p>
+          <span>40 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 4">
+          <p>Option 4</p>
+          <span>50 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 5">
+          <p>Option 5</p>
+          <span>60 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 6">
+          <p>Option 6</p>
+          <span>70 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 7">
+          <p>Option 7</p>
+          <span>80 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 8">
+          <p>Option 8</p>
+          <span>90 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 9">
+          <p>Option 9</p>
+          <span>100 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 7">
+          <p>Option 7</p>
+          <span>80 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 8">
+          <p>Option 8</p>
+          <span>90 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 9">
+          <p>Option 9</p>
+          <span>100 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 7">
+          <p>Option 7</p>
+          <span>80 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 8">
+          <p>Option 8</p>
+          <span>90 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 9">
+          <p>Option 9</p>
+          <span>100 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 7">
+          <p>Option 7</p>
+          <span>80 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 8">
+          <p>Option 8</p>
+          <span>90 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 9">
+          <p>Option 9</p>
+          <span>100 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 7">
+          <p>Option 7</p>
+          <span>80 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 8">
+          <p>Option 8</p>
+          <span>90 €</span>
+        </div>
+        <div class="option">
+          <img src="../../medias/boisnoir.jpeg" alt="Option 9">
+          <p>Option 9</p>
+          <span>100 €</span>
+        </div>
+        
+      </section>
+
+      <div class="footer">
+        <p>Total : <span>899 €</span></p>
+        <div class="buttons">
+          <button class="btn-retour">Retour</button>
+          <button class="btn-suivant">Suivant</button>
+        </div>
+      </div>
+    </div>
+
+    <!-- Colonne de droite -->
+    <div class="right-column">
+      <section class="main-display">
+        <div class="buttons">
+          <button class="btn-aide">Besoin d'aide ?</button>
+          <button class="btn-abandonner">Abandonner</button>
+        </div>
+        <img src="../../medias/boisnoir.jpeg" alt="Armoire">
+      </section>
+    </div>
+  </div>
+</main>
+
+</body>
+
+
+</html>
---
+++ squelette/footer.php
@@ -0,0 +1,30 @@
+<link href="../../dist/output.css" rel="stylesheet">
+  
+  <footer class="bg-[#E3D1C8] py-4  mt-10">
+    <div class="flex justify-center ">
+      <div class="grid grid-cols-3 items-center">
+        <!-- Image du logo -->
+        <img src="../../medias/logo_trasparent-decodumonde-2.png" alt="logo" class="w-[250px] h-auto mr-40">
+        
+        <!-- Section adresse et contact -->
+        <div class="be-vietnam-pro-bold mx-auto text-sm">
+          <p class="my-6">Adresse : 76 Av. Lenine, 93380 Pierrefitte-Sur-Seine</p>
+          <p class="my-6">E-mail : <span class="underline">decorient@gmail.com</p>
+          <p class="my-6">Téléphone : 01 48 22 98 05</p>
+        </div>
+        
+        <!-- Navigation -->
+        <nav class="be-vietnam-pro-bold mx-auto text-sm">
+          <ul>
+            <li class="my-6"><a href="../pages/index.php">Accueil</a></li>
+            <li class="my-6"><a href="../EtapesPersonnalisation/etape1.php">Personnalisation</a></li>
+            <li class="my-6"><a href="../pages/">Commande</a></li>
+            <li class="my-6"><a href="../pages/">Besoin d'aide ?</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    
+    <!-- Mention légale -->
+    <p class="text-center mt-4 text-xs">© 2024 Deco du Monde - Tous droits réservé</p>
+  </footer>
